ce05880784d2061dc071cc0e4cc2b2f1313979ad
Jazzlark <jazzlark@aol.com>
2013-09-25 14:24:25 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
-require 'rubygems'
+
+  require 'rubygems'
 require 'sinatra'
+require 'pry'
 
 set :sessions, true
 
@@ -22,7 +24,7 @@ helpers do
 
   #correct for Aces
   arr.select{|element| element == "A"}.count.times do
-    break if total <= 21
+    break if total <= BLACKJACK_AMOUNT
     total -= 10
   end
 
@@ -44,6 +46,8 @@ helpers do
       when 'Q' then 'queen'
       when 'K' then 'king'
       when 'A' then 'ace'
+    end
+  end
 
   "<img src='/images/cards/#{suit}_#{value}.jpg' class= 'card_image'>"
 end
@@ -51,28 +55,27 @@ end
 def winner! (msg)
   @play_again = true
   @show_hit_or_stay_buttons = false
-  session [:player_pot = session [:player_pot] + session[:player_bet]
-  @success = "<strong>#{session[:player_name]} wins! </strong> #{msg}"
+  session[:player_pot]= session[:player_pot] + session[:player_bet]
+  @winner = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
 end
 
-def loser (msg)
+def loser!(msg)
   @play_again = true
   @show_hit_or_stay_buttons = false
-  @success = "<strong>#{session[:player_name]} loses. </strong> #{msg}"
+  session[:player_pot] = session[:player_pot] - session[:player_bet]
+  @loser = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
 end
 
 def tie (msg)
   @play_again = true
   @show_hit_or_stay_buttons = false
-  @success = "<strong>#{session[:player_name]} It's a tie!! </strong> #{msg}"
+  @swinner = "<strong>It's a tie!! </strong> #{msg}"
   end
 end
 
 before do
   @show_hit_or_stay_buttons = true
 end
-
-
 
 get '/' do
   if session[:player_name]
@@ -88,12 +91,13 @@ get '/new_player' do
 end
 
 post '/new_player' do
+
  if params[:player_name].empty?  
   @error = "Name is required"
   halt erb(:new_player)
 end
 
-session[:player_name = params[:player_name]
+session[:player_name] = params[:player_name]
 redirect '/bet'
 end
 
@@ -102,22 +106,22 @@ get '/bet' do
   erb :bet
 end
 
-post '/bet' 
-if params [:bet_amount].nil? || params[:bet_amount].to_i == 0
-  @error = "Must make a bet!"
-  halt erb (:bet)
-elsif params [:bet_amount].to_i > session[:player_pot]
+post '/bet' do 
+if params[:bet_amount].nil? || params[:bet_amount].to_i == 0
+  @error = "Must make a bet."
+  halt erb(:bet)
+elsif params[:bet_amount].to_i > session[:player_pot]
   @error = "Bet amount cannot be greater than what you have ($#{session[player_pot]}"
 halt erb (:bet)
 else #happy path
-  session[:player_bet] = params
-  [:bet_amount].to_i
+  session[:player_bet] = params[:bet_amount].to_i
   redirect '/game'
   end
 end
 
 get '/game' do
-  session [:turn] = session [:player_name]
+  session[:turn] = session[:player_name]
+
   #create a deck and put it in session
   suits = ['H', 'D', 'C', 'S']
   values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -125,46 +129,47 @@ get '/game' do
 
   #deal cards
   session[:dealer_cards] = []
-  session[:player_name] = []
-  session[:dealer_cards] << session[deck].pop
-  session[:player_name]  << session[deck].pop
-  session[:dealer_cards] << session[deck].pop
-  session[:player_name]  << session[deck].pop
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
 
   erb :game
 end
 
 post '/game/player/hit' do
-session[:player_name]  << session[deck].pop
+session[:player_cards]  << session[:deck].pop
 
   player_total = calculate_total(session[:player_cards])
   if player_total == BLACKJACK_AMOUNT
-    winner! (#{session[:player_name]} hit blackjack!)
+    winner! ("#{session[:player_name]} hit blackjack!")
   elsif player_total > BLACKJACK_AMOUNT
-    @error = (loser!#{session[:player_name]} is busted at #{player_total")
-  end
+    loser!("It looks like #{session[:player_name]} busted at #{player_total}.")
+end
 
-  erb: game
+  erb :game, layout: false
 end
 
 post '/game/player/stay' do
-  @success = "#{session[:player_name]} has chosen to stay!"
+  @success = "#{session[:player_name]} has chosen to stay."
   @show_hit_or_stay_buttons = false
   redirect '/game/dealer'
-end
+  end
 
 get '/game/dealer' do
-  session [:turn] = "dealer"
+  session[:turn] = "dealer"
   @show_hit_or_stay_buttons = false
   
   #decision tree
 
   dealer_total = calculate_total (session[:dealer_cards])
 
-  if dealer_total == BLACKJACK_AMOUNT Loser! ("Dealer hit blackjack!")
-  elsif dealer_total > BLACKJACK_AMOUNT Winner! ("Dealer busteed at #{dealer_total}")
+  if dealer_total == BLACKJACK_AMOUNT 
+    loser!("Dealer hit blackjack!")
+  elsif dealer_total > BLACKJACK_AMOUNT 
+    winner!("Dealer busteed at #{dealer_total}.")
   elsif dealer_total >= DEALER_MIN_HIT #17, 18, 19, 20 
-
     #dealer stays
     redirect '/game/compare'
   else
@@ -172,33 +177,48 @@ get '/game/dealer' do
     @show_hit_or_stay_buttons = true
   end
 
-  erb :game
+  erb :game, layout: false
 end
 
 post '/game/dealer/hit' do
-  session [:dealer_cards] << session [:deck].pop
+  session[:dealer_cards] << session[:deck].pop
   redirect '/game/dealer'
 end
 
 get '/game/compare' do
   @show_hit_or_stay_buttons= false
 
-  player_total = calculate_total (session[:player_cards])
-  dealer_total = calculate_total (session[:dealer_total])
+  player_total = calculate_total(session[:player_cards])
+  dealer_total = calculate_total(session[:dealer_cards])
 
   if player_total < dealer_total 
-  loser! ("#{session[:player_name"]} stayed at #{player_total} and the dealer stayed at #{dealer_total}.")
-else 
+  loser! ("#{session[:player_name]} stayed at #{player_total}, and the dealer stayed at #{dealer_total}.")
+
+elsif player_total > dealer_total 
   winner! ("#{session[:player_name]} stayed at #{player_total}, and the dealer stayed at #{dealer_total}")
 else 
   tie! ("Both #{session[:player_name]} and dealer stayed at #{player_total}.")
 end
 
-erb :game
+erb :game, layout: false
+end
 
 get '/game_over' do 
   erb :game_over
+end
 
+
+
+
+
+
+
+
+
+ 
+
+
+  
 
 
 
